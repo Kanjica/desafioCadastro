9788ab1f6a4f3358e198f6d41c28d8026bc0c9b8
Kanjica <joaopedro24960@gmail.com>
2025-11-02 21:01:03 -0300

    feat(Passo1): cria formulario.json e printa no main

--- src/main/java/com/kanjica/App.java
+++ src/main/java/com/kanjica/App.java
@@ -1,13 +1,21 @@
 package com.kanjica;
+import java.io.InputStream;
+import java.util.List;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        InputStream is = App.class.getClassLoader().getResourceAsStream("formulario.json");
+
+        if (is == null) {
+            System.out.println("Arquivo não encontrado no resources!");
+            return;
+        }
+
+        List<String> perguntas = mapper.readValue(is, new TypeReference<List<String>>(){});
+        perguntas.forEach(System.out::println);
     }
 }
